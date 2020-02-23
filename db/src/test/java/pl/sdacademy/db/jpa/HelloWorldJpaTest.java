5827782cc7909e392715d23b3527376161ed5c5c
V47U4 <Bespokelease5%>
2020-02-23 14:04:24 +0100

    Add updateProductTest

--- db/src/test/java/pl/sdacademy/db/jpa/HelloWorldJpaTest.java
+++ db/src/test/java/pl/sdacademy/db/jpa/HelloWorldJpaTest.java
@@ -41,14 +41,14 @@ public class HelloWorldJpaTest {
 	void nameAddedTest() {
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
-		Product ps5Console = new Product("PS5 Console",500.00);
+		Product ps5Console = new Product("PS5 Console", 500.00);
 		entityManager.persist(ps5Console);
-		Product ps4Console = new Product("PS4 Console",650.00);
+		Product ps4Console = new Product("PS4 Console", 650.00);
 		entityManager.persist(ps4Console);
 		transaction.commit();
 		TypedQuery<Product> query = entityManager.createQuery("SELECT p FROM Product p WHERE p.name=:name",
 			Product.class);
-		query.setParameter("name","PS4 Console");
+		query.setParameter("name", "PS4 Console");
 		List<Product> resultList = query.getResultList();
 
 		assertThat(resultList).containsOnly(ps4Console);
@@ -59,15 +59,31 @@ public class HelloWorldJpaTest {
 	void findByPriceTest() {
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
-		Product ps5Console = new Product("PS5 Console",800.00);
+		Product ps5Console = new Product("PS5 Console", 800.00);
 		entityManager.persist(ps5Console);
-		Product ps4Console = new Product("PS4 Console",650.00);
+		Product ps4Console = new Product("PS4 Console", 650.00);
 		entityManager.persist(ps4Console);
 		transaction.commit();
 
-		TypedQuery<Product> query = entityManager.createNamedQuery("Product.searchByPrice",Product.class);
-		query.setParameter("price",650.00);
+		TypedQuery<Product> query = entityManager.createNamedQuery("Product.searchByPrice", Product.class);
+		query.setParameter("price", 650.00);
 		List<Product> resultList = query.getResultList();
 		assertThat(resultList).containsOnly(ps4Console);
+	}
+
+	@Test
+	@DisplayName("show how to update product")
+	void updateProductTest() {
+		EntityTransaction persistProductTransaction = entityManager.getTransaction();
+		persistProductTransaction.begin();
+		Product ps6Console = new Product("PS6 Console", 1000.00);
+		entityManager.persist(ps6Console);
+		persistProductTransaction.commit();
+
+		EntityTransaction updateNameTransaction = entityManager.getTransaction();
+		updateNameTransaction.begin();
+		Product product = entityManager.find(Product.class, ps6Console.getId());
+		product.setName("Xbox");
+		updateNameTransaction.commit();
 	}
 }
